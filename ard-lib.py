5ee6e7d045c4de171e218da1b7fca2884ed6ff63
lagertonne <mail@lagertonne.de>
2025-02-24 21:46:30 +0100

    Initial, commiit

--- ard-lib.py
+++ ard-lib.py
@@ -10,7 +10,7 @@ def search_book(query, author="", page=0):
 		"tx_drahsdbsearch_hsdbsearch[__referrer][@action]": "search",
 		"tx_drahsdbsearch_hsdbsearch[__referrer][arguments]": "YTowOnt9cd119c62bc86ce05d569a006db4286ede1e5921d",
 		"tx_drahsdbsearch_hsdbsearch[__referrer][@request]": "{\"@extension\":\"DraHsdbsearch\",\"@controller\":\"Search\",\"@action\":\"search\"}1d294c1daabcc5557450341c6e27478b9df8a433",
-		"tx_drahsdbsearch_hsdbsearch[__trustedProperties]": "{\"search\":{\"sort\":1,\"sortkey\":1,\"showhits\":1,\"erweitert\":1,\"ueberall\":1,\"hoerbar\":1,\"bestand\":1,\"titel\":1,\"mehrteilertitel\":1,\"autor\":1,\"regie\":1,\"sprecher\":1,\"mitwirkende\":1,\"gattungmulti\":1,\"gattungop\":1,\"gattung\":1,\"produzent\":1,\"preise\":1,\"pjahr\":1,\"erstsendungvon\":1,\"erstsendungbis\":1,\"dauervon\":1,\"dauerbis\":1,\"bnr\":1,\"ttv\":1}}6dc5d321cbc8f443a2e314453b0bd5ebdb215f4c",
+		"tx_drahsdbsearch_hsdbsearch[__trustedProperties]": "{\"search\":{\"bestand\":1,\"ueberall\":1,\"titel\":1,\"autor\":1,\"regie\":1,\"sprecher\":1,\"mitwirkende\":1,\"gattung\":1,\"gattungop\":1,\"gattungmulti\":1,\"produzent\":1,\"preise\":1,\"pjahr\":1,\"erstsendungvon\":1,\"erstsendungbis\":1,\"dauervon\":1,\"dauerbis\":1,\"bnr\":1,\"ttv\":1,\"erweitert\":1,\"hoerbar\":1,\"pager\":1,\"mehrteilertitel\":1,\"sort\":1,\"sortkey\":1,\"seite\":1,\"showhits\":1}}9e4ad30c60f8d96ac881218e44ea65875f55b11d",
 		"tx_drahsdbsearch_hsdbsearch[search][sort]": "",
 		"tx_drahsdbsearch_hsdbsearch[search][sortkey]": "",
 		"tx_drahsdbsearch_hsdbsearch[search][showhits]": "10",
@@ -48,11 +48,12 @@ def search_book(query, author="", page=0):
 		# "tx_drahsdbsearch_hsdbsearch[search][mehrteilertitel]": "",
 		# "tx_drahsdbsearch_hsdbsearch[search][sort]": "",
 		# "tx_drahsdbsearch_hsdbsearch[search][sortkey]": "dat",
-		#"tx_drahsdbsearch_hsdbsearch[search][seite]": "1",
+		"tx_drahsdbsearch_hsdbsearch[search][seite]": str(page),
 	}
 
 	search_url = "https://hoerspiele.dra.de/suche/"
 
+	# Make initial request
 	search_results = requests.post(search_url, data=search_params)
 
 	if search_results.status_code == 200:
@@ -60,24 +61,42 @@ def search_book(query, author="", page=0):
 
 	results = soup.find('table', class_='resultTable').find('tbody').find_all('tr')
 
-	page_count = soup.find('div', class_='pager').get_text().strip()
-
-	print(page_count)
+	# We waste one request to get the page count
+	page_count = len(soup.find('div', class_='pager').get_text().strip().splitlines())
 
 	audiobook_results = []
 
-	for row in results:
-		book = {}
-		parsed_row = row.find_all('td')
-		# Titel, Autor, Produktion, Datum, Gattung
-		book["title"] = parsed_row[0].get_text()
-		book["author"] = parsed_row[1].get_text()
-		book["production"] = parsed_row[2].get_text()
-		book["date"] = parsed_row[3].get_text()
-		book["type"] = parsed_row[4].find('img').get('title')
-		audiobook_results.append(book)
+	for page_num in range(0, page_count):
+		page = page_num
+		search_url = "https://hoerspiele.dra.de/suche/"
+
+		# Make initial request
+		search_results = requests.post(search_url, data=search_params)
+
+		if search_results.status_code == 200:
+			soup = BeautifulSoup(search_results.content, "html.parser")
+
+		results = soup.find('table', class_='resultTable').find('tbody').find_all('tr')
+
+		for row in results:
+			book = {}
+			parsed_row = row.find_all('td')
+			# Titel, Autor, Produktion, Datum, Gattung
+			book["title"] = parsed_row[0].get_text()
+			book["author"] = parsed_row[1].get_text()
+			book["production"] = parsed_row[2].get_text()
+			book["date"] = parsed_row[3].get_text()
+			book["type"] = parsed_row[4].find('img').get('title')
+			audiobook_results.append(book)
+
+	page_count = len(soup.find('div', class_='pager').get_text().strip().splitlines())
+	print(page_count)
+
 
 	return audiobook_results
 
 
-pprint.pprint(search_book(query="Steppenwolf"))
+result_books = search_book(query="Steppenwolf")
+pprint.pprint(result_books)
+print(len(result_books))
+
